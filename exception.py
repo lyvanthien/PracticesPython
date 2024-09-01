4a0f8b9dd6497ff144f351454c828e7165c0a52b
lythienhoang <lythienhoang@gmail.com>
2024-09-01 17:53:16 +0700

    I have just completed Test4.py and exception.py files.

---
+++ exception.py
@@ -0,0 +1,27 @@
+# exception = events detected during execution that interrrupt the flow of a progranm
+
+"""
+numerator = int(input(("Enter a number to divide: ")))
+denominator = int(input("Enter a number to divide by: "))
+result = numerator / denominator
+print(result)
+"""
+
+try:
+    numerator = int(input(("Enter a number to divide: ")))
+    denominator = int(input("Enter a number to divide by: "))
+    result = numerator / denominator
+
+except ZeroDivisionError as e:
+    print(e)
+    print("You can't divide by zero! idiot!")
+except ValueError as e:
+    print(e)
+    print("Enter only numbers plz")
+except Exception as e:
+    print(e)
+    print("something went wrong :(")
+else:
+    print(result)
+finally:
+    print("This will always execute")
