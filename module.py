0e11c8536cc7e9cc20205977c9cdda0222ec1773
lythienhoang <lythienhoang@gmail.com>
2024-09-06 21:05:00 +0700

    I have just completed randomGame.py file.

---
+++ module.py
@@ -0,0 +1,24 @@
+# module = a file containing python code. May contain functions, classes, etc.
+# Used with modular programming, which is to separate a program into parts
+
+"""
+import messages
+
+messages.hello()
+messages.bye()
+"""
+"""
+import messages as msg
+
+msg.hello()
+msg.bye()
+"""
+
+"""
+# from messages import hello,bye
+from messages import *
+hello()
+bye()
+"""
+
+help("modules")
